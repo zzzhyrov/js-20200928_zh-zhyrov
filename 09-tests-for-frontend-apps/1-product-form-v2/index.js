5d96579086bfbaf7eee5c7c22ca85f8b44075977
Yevhenii <zh.zhyrov@gmail.com>
2020-11-07 16:38:00 +0200

    09-1-product-form-v2: add code from mentor_s solution (rewrite)

--- 09-tests-for-frontend-apps/1-product-form-v2/index.js
+++ 09-tests-for-frontend-apps/1-product-form-v2/index.js
@@ -1,4 +1,4 @@
-import SortableList from '../../2-sortable-list/solution/index.js';
+import SortableList from '../2-sortable-list/index.js';
 import escapeHtml from './utils/escape-html.js';
 import fetchJson from './utils/fetch-json.js';
 
